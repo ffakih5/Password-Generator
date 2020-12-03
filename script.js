1b034b9f62a3494f8417a11d890208dcb5dc05c9
Fatima Fakih <fatima.fakih@hotmail.com.au>
2020-12-04 09:33:24 +1100

    ordering criteria list to make sense for me

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 // Assignment Code
+-generate password button is pressed, followed by series of prompts .
+-prompts ask which criteria is needed:
+-(password length; upper/lowercase, special characters)
+-upper/owercase, special characters in an array
+-input stored in variable per prompt
+-validation used to ensure user criteria is met
+-Once prompts are done, random number generator runs over each array to oputput a password at the selected length  At the end of the prompts, a random number generator needs to run over each array and output a password the length selected.
+-password returned, output in the password form 
+-varaibles: passwordLength, lowercaseChar, uppercaseChar, specialChar, 
+-random number generator; iterate the number of times stored in the password length variable
+-Function- generate password = generatePassword();
+-generatePassword() will return password to the password variable //
+
 var generateBtn = document.querySelector("#generate");
 function promptCriteria() { 
-   
+  /* 
     do  {  
         var passwordLength = prompt("password length")
         var passwordLengthNumber = parseInt(passwordLength);
@@ -11,12 +24,12 @@ function promptCriteria() {
 }
 function generatePassword() {
    var passwordLength=  promptCriteria() 
-    return "password" + passwordLength
+    return "password" + passwordLength  */
 
 }
 // Write password to the #password input
 function writePassword() { 
-  
+  var 
   var passwordText = document.querySelector("#password");
   var password = generatePassword();
 
