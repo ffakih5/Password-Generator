d2c4950db8427c5e91082a57e8c183b8ecb14061
Fatima Fakih <fatima.fakih@hotmail.com.au>
2020-12-04 13:50:32 +1100

    can't loop

--- script.js
+++ script.js
@@ -2,94 +2,55 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() { 
+
   var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
   var lowercaseChar = "abcdefghijklmnopqrstuvwxyz"
   var numericChar= "0123456789"
   var specialChar = "!@#$%^&*()_-+={}[];:'`~<,>.?/|"
   var passwordLength; 
-  var userCharacters= ""
-  var userCharactersIndex = 0
+  var uppercaseValid;
+  var numericValid;
+  var specialValid;
 
-  for (var i= 0; i < 1; i++) {
-    var passwordLength = parseInt(prompt ("Please select a password length\n"))
-    if (isNaN(passwordLength) === true || passwordLength >= 8 &&  passwordLength <= 128) {
-      var errorLength = confirm("Please select a numerical value between 8 and 128") 
-      if (errorLength === true) {
-        i -= 1 ;
+
+  function confirmLength(){
+    passwordLength = prompt("Please select a password length (between 8-128 characters): ");
+  
+      if (passwordLength<8){
+        alert("Password length must be a number between 8-128 characters");
+        confirmLength();
+      }else if (passwordLength>128){
+        alert("Password length must be a number between 8-128 characters");
+        confirmLength();
+      }else if (isNaN(passwordLength)){
+        alert("Password length must be a number between 8-128 characters");
+        confirmLength();
+      }else{
+      alert("Please answer the following prompts to acquire the most suited criteria for your password.\n");
       }
-    }
+      return passwordLength;
   }
-  for (var i= 0; i < 1; i++) {
-        var useLowercase = confirm ("Would you like to use lower case letters in your password? "); 
-     if (useLowercase) {
-           userCharacters += lowercaseChar; 
-        }
-        if (useLowercase) { 
-          userCharctersIndex +=26;
-          }
-          console.log(userCharacters); 
-          console.log(userCharactersIndex); 
-
-      var userUppercase = confirm ("Would you like to use uppercase letters in your password?");
-      if (useUppercase) {
-        userCharacters += uppercaseChar
-     }
-     if (useUppercase) { 
-       userCharctersIndex +=26;
-       } 
-    }
-
-
-
-
-
-        
-
+  function confirmUppercase(){
+    uppercaseCheck = prompt("Would you like to include uppercase letters in your password? \n(Yes or No)");
+      uppercaseValid = uppercaseValid.toLowerCase();
+  
+      if (uppercaseValid === null || uppercaseValid === ""){
+        alert("Please answer Yes or No");
+        confirmUppercase();
+  
+      }else if (uppercaseValid === "yes" || uppercaseValid ==="y"){
+        uppercaseCheck = true;
+        return uppercaseCheck;
+  
+      }else if (uppercaseValid === "no" || uppercaseValid ==="n"){
+        uppercaseValid = false;
+        return uppercaseValid;
       
-    }
-    
-    
-    
-    
-    
-    
-  
-  
-  
-  }
-
-
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-    passwordText.value = password;
-
-
-    console.log(passwordLength); 
-
-
+      }else {
+        alert("Please answer Yes or No");
+        confirmUppercase();
+      }
+      return uppercaseValid;
   }
   
-
-
-
-
-
-
-
-
   
-  
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
- 
- 
- 
- 
- 
- 
- 
-  
-    
